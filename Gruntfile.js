ef61a68e1d7a50ad6582fc1b59af2be1d4564b19
Richard Still <richard.still@ft.com>
2014-08-11 17:03:44 +0100

    First pass at something that makes a very simple route

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,59 +2,14 @@ module.exports = function(grunt) {
   "use strict";
 
   grunt.initConfig({
-    sass: {
-      docs: {
-        options: {
-          style: 'compressed',
-          loadPath: './app'
-        },
-        files: {
-          './static/bundle.css': './app/main/scss/main.scss'
-        }
-      }
-    },
-    browserify: {
-      dist: {
-        files: {
-          './static/bundle.js': ['./app/main/js/main.js'],
-        },
-        options: {
-          transform: ['debowerify', 'textrequireify']
-        }
-      }
-    },
     watch: {
-      sass: {
-        files: ['./app/scss/**'],
-        tasks: ['sass']
-      },
       js: {
         files: ['./app/js/**'],
         tasks: ['browserify']
       }
-    },
-    copy: {
-      resources: {
-        files: [
-          {
-            src: '**/*',
-            dest: './static/o-ft-header/img/',
-            expand: true,
-            cwd: './app/o-ft-header/img'
-          }
-        ]
-      }
     }
   });
 
-
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-browserify');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-
   // Default task(s).
-  grunt.registerTask('default', ['sass', 'browserify', 'copy']);
-  grunt.registerTask('js', ['browserify']);
-  grunt.registerTask('css', ['sass']);
+  grunt.registerTask('default', ['lint']);
 };
--- server/app.js
+++ server/app.js
@@ -2,14 +2,16 @@
 
 var express         = require('express'),
     middleware      = require('./middleware.js'),
-    mustache        = require('hogan-express'),
-    os              = require('os'),
     compression     = require('compression')(),
-    config          = require('./config.js'),
-    ftApi           = require('ft-api-client'),
-    flags           = require('./flags');
+    resolveRoute   = require('./controllers/resolveRoute.js'),
+    config          = require('./config.js');
 
 var app = express();
+
+// Setup the routes
+var router = express.Router();
+router.use(resolveRoute);
+app.use('/', router);
 
 // Enable output compression
 app.use(compression);
@@ -17,53 +19,7 @@ app.use(compression);
 // Set CORS headers
 app.use(middleware.allowCrossDomain);
 
-// Using mustache via Hogan
-app.set('view engine', 'mustache');
-
-// Set the default, parent template
-app.set('layout', 'layout');
-
-// Set the partials available to all templates
-app.set('partials', {header: 'partials/header', head: 'partials/head', footer: 'partials/footer'});
-
-// Set the default location for templates
-app.set('views', 'templates');
-
-// Set mustache (hogan) as the rendering engine
-app.engine('mustache', mustache);
-
-// Static resources folder
-app.use(express.static('./static'));
-
-var ft = new ftApi({ apiKey: process.env.FT_API_KEY });
-
-var flagUrl = 'http://' + os.hostname() + ':' + config.PORT + '/__flags';
-
-console.log(flagUrl);
-flags.hydrate(flagUrl);
-
-// Setup the routes
-app.get('/', function (req, res) {
-    res.render('body', {title: 'Hello world', content: 'Hello worlds', flags: flags.get() });
-});
-
-app.get('/news/:id', function (req, res) {
-    ft.getItem(req.params.id, function (err, response) {
-        if (err) {
-            res.send(404, err);
-        } else {
-            res.render('body', {
-                title: response.item.title.title,
-                content: response.item.body.body
-            });
-        }
-    });
-});
-
-// dummy flag service
-app.get('/__flags', function (req, res) {
-    res.send(200, '{ "foo": true, "boo": false }');
-});
+// Utility end points
 app.get('/__health', require('./controllers/health'));
 app.get('/__metrics', require('./controllers/metrics'));
 
