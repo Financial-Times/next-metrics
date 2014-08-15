55024e3b732f38242e4ebb5cf3fd674e9d2b16a8
Matt Chadburn <matt.chadburn@ft.com>
2014-08-15 23:02:27 +0100

    Express be gone

--- app/main/js/main.js
+++
@@ -1,2 +0,0 @@
-// This module does not need a handler. It executes on parse. So naughty.
-require('o-ft-header');
--- server/controllers/health.js
+++
@@ -1,9 +0,0 @@
-// __health code
-'use strict';
-var healthStatus = {
-	status: 'OK'
-};
-
-module.exports = function (req, res) {
-	res.jsonp(healthStatus);
-};
