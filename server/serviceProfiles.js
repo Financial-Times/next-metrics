cce288f9113bec71adb33970bcc0c14db770df16
Richard Still <richard.still@ft.com>
2014-08-20 16:23:27 +0100

    Added polling for service data and modified tests accordingly

--- server/serviceProfiles.js
+++ server/serviceProfiles.js
@@ -1,56 +1,17 @@
 'use strict';
+GLOBAL.Promise = require('es6-promise').Promise;
+var Poller = require('ft-poller');
 
-var routeProfiles = [{
-	name: "badger",
-	path: "badger", // A regex to match the high level traffic (possibly use a service to resolve a page to a type of thing and route accordingly.)
-	desc: "A service that creates perfectly formed badgers",
-	versions: {
-		"#123": {
-			nodes: [
-				"next-router-test-app-badger-1.herokuapp.com"
-			],
-			isPrimary: true  // Indicates which service is the default to receive traffic
-		},
-		"#234": {
-			nodes: [
-				"next-router-test-app-bodger-1.herokuapp.com"
-			],
-			filters: {
-				//"load": 5%, // take this percentage of...
-				"http.User-Agent": "Nexus 4", // this regex pattern to match against the  user agent
-				"rateLimitTo": 10, //  request p/sec
-				"http.Cookie": {
-					"cookieconsent": "seen"
-				}, // a regex to test against the cookie value
-				"http.x-headers": {
-                    "x-foo": 'hello',
-                    "x-boo": 'world'
-                },
-                "geo.country": "uk",
-				"deny": "all",
-				"access": "129.412.12.32"
-			}
-		}
-	}
-},
-{
-	name: "newsfeed",
-	path: "newsfeed", // A regex to match the high level traffic (possibly use a service to resolve a page to a type of thing and route accordingly.)
-	desc: "A service that creates perfectly formed badgers",
-	versions: {
-		"#123": {
-			nodes: [
-				"newsfeed.ft.com"
-			],
-			isPrimary: true  // Indicates which service is the default to receive traffic
-		}
-	}
-}
+// Poll the service registry for service data
+var getServiceProfiles = function (cfg) {
+	var profileFetcher =  new Poller({
+	    url: cfg.url, 
+	    refreshInterval: 10000,
+	    parseData: cfg.cb // When the data arrives invoke the passed callback
+	});
 
-];
+	// Start polling requesting an immediate fetch of data
+	profileFetcher.start({ initialRequest: true });	
+};
 
-function getProfiles () {
-	return routeProfiles;
-}
-
-module.exports.getProfiles = getProfiles;
+module.exports.getServiceProfiles = getServiceProfiles;
--- tests/routing.js
+++ tests/routing.js
@@ -1,4 +1,5 @@
-
+/* global describe, it, beforeEach, afterEach */
+'use strict';
 var request = require('superagent'),
     mocha = require('mocha'),
     fs = require('fs'),
@@ -11,13 +12,22 @@ describe('Router', function() {
     var host = 'http://localhost:5000',
         server;
 
+    before(function (done) {
+      var mock = nock('http://next-service-registry.herokuapp.com').get('/services').reply(200, '');
+      //require('./mockProfileData.js').getProfiles()
+      // FIXME figure out a better way to wait for the app to be ready than introducing a delay 
+      setTimeout(function () {
+          done();
+        }, 500);
+    });
+
     beforeEach(function () {
         server = app.listen(5000);
-    })
+    });
     
     afterEach(function () {
         server.close();
-    })
+    });
 
     describe('Service', function () {
 
@@ -27,24 +37,24 @@ describe('Router', function() {
                     expect(res.status).to.equal(200);
                     expect(mock.isDone()).to.be.true;
                     done();
-            })
-        })
+            });
+        });
         
         it('Respond with a not found message when requesting an invalid service path', function (done) {
             var mock = nock('http://next-router-test-app-badger-1.herokuapp.com').get('/four-oh-four').reply(404, '');
             request.get(host + '/four-oh-four').end(function (err, res) {
                     expect(res.status).to.equal(404);
                     done();
-            })
-        })
+            });
+        });
         
         it('Vary by service version', function (done) {
             var mock = nock('http://next-router-test-app-badger-1.herokuapp.com').get('/badger').reply(200, '');
             request.get(host + '/badger').end(function (err, res) {
                     expect(res.headers.vary).to.contain('X-Version');
                     done();
-            })
-        })
+            });
+        });
 
     });
 
@@ -58,8 +68,8 @@ describe('Router', function() {
                       expect(res.header['x-version']).to.equal('#234');
                       expect(res.status).to.equal(200);
                       done();
-            })
-        })
+            });
+        });
         
         it('by user-agent', function (done) {
             var mock = nock('http://next-router-test-app-bodger-1.herokuapp.com').get('/badger').reply(200, '');
@@ -69,8 +79,8 @@ describe('Router', function() {
                       expect(res.header['x-version']).to.equal('#234');
                       expect(res.status).to.equal(200);
                       done();
-            })
-        })
+            });
+        });
         
         it('by x-headers', function (done) {
             var mock = nock('http://next-router-test-app-bodger-1.herokuapp.com').get('/badger').reply(200, '');
@@ -80,8 +90,8 @@ describe('Router', function() {
                       expect(res.header['x-version']).to.equal('#234');
                       expect(res.status).to.equal(200);
                       done();
-            })
-        })
+            });
+        });
         
         it('Respond with a not found message when a specified filter does not exist', function (done) {
             var mock = nock('http://next-router-test-app-badger-1.herokuapp.com').get('/badger').reply(404, '');
@@ -90,9 +100,8 @@ describe('Router', function() {
                    .end(function (err, res) {
                       expect(res.status).to.equal(404);
                       done();
-            })
-        })
-        
-
+            });
+        });
+      
     });
 });
