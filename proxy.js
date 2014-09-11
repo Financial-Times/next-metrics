a0689b4d9751fc7597f9fba742630d0c3f0a494f
Matt Chadburn <matt.chadburn@ft.com>
2014-09-11 14:08:00 +0100

    request logged to diagnose heroku problem

--- proxy.js
+++ proxy.js
@@ -42,7 +42,7 @@ var server = http.createServer(function(req, res) {
     */
 
     // instrument the req & response object 
-    metrics.instrument(req, { as: 'http.req' })
+    metrics.instrument(req, { as: 'http.req' });
     metrics.instrument(res, { as: 'http.res' });
 
     // If we have no service data then don't even try to route a request. Seriously don't.
