720ed08a1611e8311ec6b9bcce293e6b43c0bbba
Matthew Andrews <matt@mattandre.ws>
2014-10-10 15:16:51 +0100

    Hack it so that CLI always uses global version

--- bin/cli.sh
+++ bin/cli.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-make --makefile "${DIR}/../Makefile" --directory "${DIR}/../" run
+NODE_PATH=$( dirname "$( npm bin --g )" )
+NEXT_ROUTER="${NODE_PATH}/lib/node_modules/next-router/"
+make --makefile "${NEXT_ROUTER}Makefile" --directory "${NEXT_ROUTER}" run
