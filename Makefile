66584dc346de4ba646653b2a37b999c313d0f06b
Matt Chadburn <matt.chadburn@ft.com>
2014-11-20 16:04:20 +0000

    Remove unused code

--- Makefile
+++
@@ -1,6 +0,0 @@
-test: 
-	@./node_modules/.bin/mocha -R spec tests/routing.js
-	@./node_modules/.bin/mocha -R spec tests/serviceUnavailable.js
-
-run:
-	@node proxy.js
--- bin/cli.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-NODE_PATH=$( dirname "$( npm bin --g )" )
-NEXT_ROUTER="${NODE_PATH}/lib/node_modules/next-router/"
-make --makefile "${NEXT_ROUTER}Makefile" --directory "${NEXT_ROUTER}" run
