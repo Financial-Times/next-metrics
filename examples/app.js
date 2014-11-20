0193ca47bec9ce490eb471b8e5638a4ecc16f65d
Matt Chadburn <matt.chadburn@ft.com>
2014-11-20 18:17:27 +0000

    Instrument the correct thing

--- examples/app.js
+++ examples/app.js
@@ -8,16 +8,17 @@ var Metrics = require('../lib/metrics')({ app: 'example', flushEvery: 5000 });
 app.get('/', function (req, res) {
 
     Metrics.instrument(req, { as: 'http.req' });
-    Metrics.instrument(req, { as: 'http.res' });
+    Metrics.instrument(res, { as: 'http.res' });
     
     res.status(200).send('hello');
 
 })
 
-Metrics.instrument(server, { 'as': 'http.server' });
 
 var port = Number(process.env.PORT || 5005);
 
 var server = app.listen(port, function () {
     console.log('listening on port', port);
 })
+
+Metrics.instrument(server, { 'as': 'http.server' });
