a3ff4d8d0747a96b6516e02d1ab3ef56f33b9532
Matt Chadburn <matt.chadburn@ft.com>
2014-11-20 20:12:44 +0000

    Vary the status code and response times from the example app

--- examples/app.js
+++ examples/app.js
@@ -9,15 +9,22 @@ app.get('/', function (req, res) {
 
     Metrics.instrument(req, { as: 'http.req' });
     Metrics.instrument(res, { as: 'http.res' });
-    
-    res.status(200).send('hello');
 
+    var statii = [200, 200, 200, 201, 202, 302, 301, 402, 404, 500, 503]
+    var status = statii[Math.floor(Math.random()*statii.length)];
+
+    var responseTime = (Math.random()*2)*1000;
+    
+    setTimeout(function () {
+        res.status(status).send('hello');
+    }, responseTime);
 })
 
 
 var port = Number(process.env.PORT || 5005);
 
 var server = app.listen(port, function () {
+    Metrics.count('express.start');
     console.log('listening on port', port);
 })
 
