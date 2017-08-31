010d2116057412aee771f50e279619b75e0216d3
Gabrielle von Koss <gabivk@gmail.com>
2017-08-31 16:16:10 +0100

    updation  🐿 v2.5.16

---
+++ secret-squirrel.js
@@ -0,0 +1,14 @@
+module.exports = {
+	files: {
+		allow: [
+			'TODO'
+		],
+		allowOverrides: []
+	},
+	strings: {
+		deny: [],
+		denyOverrides: [
+			'd3fe0b06-9e43-11e3-b429-00144feab7de' // README.md:96|97|98
+		]
+	}
+};
