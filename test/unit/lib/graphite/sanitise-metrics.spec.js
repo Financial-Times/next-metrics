a716b7e3ac4ee85344109024f59bbc1ea523276e
Nayana Shetty <nayana.shetty@ft.com>
2020-08-11 11:39:20 +0100

    fixed failing test 🐿 v2.13.0

--- test/unit/lib/graphite/sanitise-metrics.spec.js
+++ test/unit/lib/graphite/sanitise-metrics.spec.js
@@ -14,13 +14,13 @@ describe('lib/graphite/sanitise-metrics', () => {
         // hardcoding the date to Epoch timestamp: 1434399121 for testing metrics inputs
 		clock = sinon.useFakeTimers(new Date('Mon, 15 Jun 2015 20:12:01 UTC').getTime());
 
-		nLogger = require('../../mock/n-logger.mock');
-		mockery.registerMock('@financial-times/n-logger', nLogger);
+        nLogger = require('../../mock/n-logger.mock');
+        mockery.registerMock('@financial-times/n-logger', nLogger);
 
     });
     
     afterEach(() => {
-		clock.restore();
+        clock.restore();
 	});
 
     const metrics = {'metric1': 123, 'metric2': 23};
@@ -64,16 +64,6 @@ describe('lib/graphite/sanitise-metrics', () => {
         });
         assert.equal(data[0],'UUID.appname.metric_name.this_is_1 123 1434399121')
     });
-    // it('a warn message with the event APP_IS_SENDING_BAD_METRICS should be logged when metrics name contains spaces', () => {
-    //     const metrics = {'metric 1': 123};
-    //     const data = sanitiseMetrics({
-    //         metrics_prefix,
-    //         metrics
-    //     });
-    //     assert.calledOnce(nLogger.default.warn);
-    //     // assert.isObject(nLogger.default.warn.firstCall.args[0]);
-    //     // assert.equal(nLogger.default.warn.firstCall.args[0].event, 'APP_IS_SENDING_BAD_METRICS');
-    // });
     it('replace metrics values that are not integers or decimals with 0', () => {
         const metrics = {'metric1': 'peer'};
         const data = sanitiseMetrics({
@@ -82,7 +72,19 @@ describe('lib/graphite/sanitise-metrics', () => {
         });
         assert.equal(data[0],'UUID.appname.metric1 0 1434399121')
     });
-    it('replace metrics name longer than 255 characters with keylengtherror. and last 5 characters of the long metrics name', () => {
+    it('a warn message with the event APP_IS_SENDING_BAD_METRICS should be logged when metrics name contains spaces', () => {
+        const metrics = {'metric 1': 123};
+        sanitiseMetrics({
+            metrics_prefix,
+            metrics
+        });
+        setTimeout(() => {
+            assert.calledOnce(nLogger.default.warn);
+            assert.isObject(nLogger.default.warn.firstCall.args[0]);
+            assert.equal(nLogger.default.warn.firstCall.args[0].event, 'APP_IS_SENDING_BAD_METRICS');
+        },0);
+    });
+    it('replace metrics name longer than 255 characters with keylengtherror.{last 5 characters of the long metrics name}', () => {
         const metrics = {'long.count.hellojshdsjdhjskcns.sdcdscdscds.vdscsdcscf.afefaewfawef.adcfadscfaedf.faedfcadfaerwf.faedfaerferrae.ferfersfserfgse.ferfaerferwf.faerfwerfweferrfhserfj.fesrfserf.frefgerferg.tfyjftygg.sdfsdfvc.dwaedcs.hfddvhh': 123};
         const data = sanitiseMetrics({
             metrics_prefix,
@@ -90,7 +92,7 @@ describe('lib/graphite/sanitise-metrics', () => {
         });
         assert.equal(data[0],'UUID.appname.keylengtherror.ddvhh 123 1434399121')
     });
-    it('replace metrics name longer than 255 characters with keylengtherror. and last 5 characters without any dots of the long metrics name', () => {
+    it('replace metrics name longer than 255 characters with keylengtherror.{last 5 characters without any dots of the long metrics name}', () => {
         const metrics = {'long.count.hellojshdsjdhjskcns.sdcdscdscds.vdscsdcscf.afefaewfawef.adcfadscfaedf.faedfcadfaerwf.faedfaerferrae.ferfersfserfgse.ferfaerferwf.faerfwerfweferrfhserfj.fesrfserf.frefgerferg.tfyjftygg.sdfsdfvc.dwaedcs.hfd.dvhh': 123};
         const data = sanitiseMetrics({
             metrics_prefix,
@@ -98,4 +100,5 @@ describe('lib/graphite/sanitise-metrics', () => {
         });
         assert.equal(data[0],'UUID.appname.keylengtherror.ddvhh 123 1434399121')
     });
+
 });
