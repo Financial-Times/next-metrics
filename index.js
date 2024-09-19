c2b2555e8673474435f9d3e1f41762dc931a804a
Rowan Manning <138944+rowanmanning@users.noreply.github.com>
2024-09-19 10:21:47 +0100

    Make the library do nothing
    This is a breaking change because the library no longer sends any
    metrics to Graphite. The entire public API has been replaced with
    properties and methods that do nothing except log a warning.
    
    This means:
    
      * Systems can stop sending metrics without having to immediately
        remove all imports of next-metrics and calls to `count` etc
    
      * Once we're ready we can mark this major version as "safe" and just
        watch all the dependency PRs get merged instead of teams having to
        manually change anything
    
      * If this library is still installed it's _way_ smaller and has fewer
        dependencies to worry about
    
    Some things to consider:
    
      * We may want to release patches to the 12.x branch if security issues
        crop up. This is fine and relatively easy to do - we'll just need to
        manually tag releases in npm to stop 12.x versions being `latest`
    
      * We'll also want a corresponding major version of n-express that
        uses this version of next-metrics.

--- index.js
+++ index.js
@@ -1,10 +1,85 @@
 'use strict';
 
-const Metrics = require('./lib/metrics');
+const logger = require('@dotcom-reliability-kit/logger');
+
+const silence = Boolean(process.env.NEXT_METRICS_SILENCE_WARNINGS);
+
+/**
+ * @template T
+ * @param {string} signature
+ * @param {T} returnValue
+ * @returns {() => T}
+ */
+function buildDeprecationLogger (signature, returnValue) {
+	return () => {
+		if (!silence) {
+			const error = new Error(`${signature} no longer does anything. Please remove it from your code`);
+			logger.warn(error, {
+				event: 'NEXT_METRICS_DEPRECATION',
+				message: `Call to ${signature} found`
+			});
+		}
+		return returnValue;
+	};
+}
+
+/**
+ * @param {function} classFn
+ * @param {string} methodName
+ * @param {any} returnValue
+ */
+function addDeprecatedMethod (classFn, methodName, returnValue = undefined) {
+	classFn.prototype[methodName] = buildDeprecationLogger(`${classFn.name}#${methodName}()`, returnValue);
+}
+
+/**
+ * @param {function} classFn
+ * @param {string} propertyName
+ * @param {any} value
+ */
+function addDeprecatedProperty (classFn, propertyName, value) {
+	Object.defineProperty(classFn.prototype, propertyName, {
+		get: buildDeprecationLogger(`${classFn.name}#${propertyName}[get]`, value),
+		set: buildDeprecationLogger(`${classFn.name}#${propertyName}[set]`, value)
+	});
+}
+
+// Create a fetcher that matches the old API but doesn't actually
+// do anything. We only instrument methods we know are used
+class Fetcher {}
+addDeprecatedMethod(Fetcher, 'instrument');
+
+// Create a metrics client that matches the old API but doesn't
+// actually do anything. We only instrument methods we know are used
+class Metrics {}
+addDeprecatedProperty(Metrics, 'opts', {});
+addDeprecatedProperty(Metrics, 'graphites', []);
+addDeprecatedProperty(Metrics, 'aggregators', []);
+addDeprecatedProperty(Metrics, 'customCounters', {});
+addDeprecatedProperty(Metrics, 'customHistograms', {});
+addDeprecatedProperty(Metrics, 'hasValidConfiguration', false);
+addDeprecatedProperty(Metrics, 'services', {});
+addDeprecatedProperty(Metrics, 'fetch', new Fetcher());
+addDeprecatedMethod(Metrics, 'init');
+addDeprecatedMethod(Metrics, 'count');
+addDeprecatedMethod(Metrics, 'histogram');
+addDeprecatedMethod(Metrics, 'flushCustomCounters');
+addDeprecatedMethod(Metrics, 'flushCustomHistograms');
+addDeprecatedMethod(Metrics, 'flushRate');
+addDeprecatedMethod(Metrics, 'setupDefaultAggregators');
+addDeprecatedMethod(Metrics, 'setupCustomAggregators');
+addDeprecatedMethod(Metrics, 'flush');
+addDeprecatedMethod(Metrics, 'instrument');
+addDeprecatedMethod(Metrics, 'registerAggregator');
+
+// Create a graphite client that matches the old API but doesn't
+// actually do anything. We only instrument methods we know are used
+class GraphiteClient {}
+addDeprecatedProperty(GraphiteClient, 'destination', { port: 137, host: 'localhost' });
+addDeprecatedProperty(GraphiteClient, 'prefix', 'deprecated');
+addDeprecatedProperty(GraphiteClient, 'noLog', false);
+addDeprecatedMethod(GraphiteClient, 'log');
 
 module.exports = new Metrics();
-
 module.exports.Metrics = Metrics;
-module.exports.GraphiteClient = require('./lib/graphite/client');
-
-module.exports.services = require('./lib/metrics/services');
+module.exports.GraphiteClient = GraphiteClient;
